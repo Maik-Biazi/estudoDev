fddac0da6712c7ccde6ad6f56c2a8502aea6db73
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-20 15:11:34 -0300

    usando ejs

--- node/app.js
+++ node/app.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express()
+app.set('view engine', 'ejs')
+
+app.get('/tecnologia',function(req, res){
+    res.render("secao/tecnologia")
+})
+
 app.get('/',function(req, res){
     res.send("<html><body>Portal de noticia</body></html>")
 })
-app.get('/tecnologia',function(req, res){
-    res.send("<html><body>noticia de Tecnologia</body></html>")
-})
+
 
 app.listen(3000, function(){
     console.log('servidor rodando com express')
