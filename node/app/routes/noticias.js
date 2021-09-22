c78d97a999424b1535360829f948efe68a700174
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-21 23:14:17 -0300

    inserindo bd

--- node/app/routes/noticias.js
+++ node/app/routes/noticias.js
@@ -1,4 +1,13 @@
 module.exports = function (app) {
+    
+    var mysql =require('mysql')
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user : 'root',
+        password: '1234',
+        database : 'portal_noticias'
+
+    })
     app.get('/noticias', function (req, res) {
         res.render("noticias/noticias")
     })
