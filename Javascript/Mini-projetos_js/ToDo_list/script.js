80bd0d49a8b329f95c8483a3249b352ab42dcbbd
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-21 21:14:15 -0300

    ajustando rotas

--- Javascript/Mini-projetos_js/ToDo_list/script.js
+++ Javascript/Mini-projetos_js/ToDo_list/script.js
@@ -1,21 +1,22 @@
-//funçao que adiciona tarefa
-function addTask(){
-    const taskTitle = document.querySelector("#task-title").value
+// funçao que adiciona tarefa
+ function addTask(){
+     const taskTitle = document.querySelector("#task-title").value
     if(taskTitle){
         //clona o template
         const template = document.querySelector(".template")
 
         const newTask = template.cloneNode(true)
+        
 
         //adiciona titulo para
         newTask.querySelector(".task-title").textContent = taskTitle
 
         //remover as classes desnecessarias
-        newTask.classList.remove("template")
-        newTask.classList.remove("hide")
+       newTask.classList.remove("template")
+      newTask.classList.remove("hide")
 
-// adciona tarefa na lista
-const list = document.querySelector("#task-list")
+//  adciona tarefa na lista
+ const list = document.querySelector("#task-list")
 list.appendChild(newTask)
     }
 }
@@ -24,8 +25,8 @@ const addBtn = document.querySelector("#add-btn")
 
 addBtn.addEventListener("click", function(e) {
     e.preventDefault();
-console.log("testando")
 
-addTask()
+    console.log("testando")
+ addTask()
 
 })
