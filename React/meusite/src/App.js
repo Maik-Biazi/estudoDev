1ab696cf01ea17c39d7f7ae793794ab773e17c45
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-22 23:17:50 -0300

    ciclo de vida

--- React/meusite/src/App.js
+++ React/meusite/src/App.js
@@ -1,35 +1,23 @@
-import React,{Component} from 'react'
-class Equipe extends Component {
-    render() {
-        return(
-            <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo}
-                idade={this.props.idade}/>
-                <hr/>
-            </div>
-        )
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={}
+        hora: '00:00:00'
     }
-}
-class Sobre extends Component {
+    componentDidMount() {
+        
+    }
     render() {
         return (
-            <div>
-            <h2> Ola sou o Matheus{this.props.nome}</h2>
-            <h2>Cargo: {this.props.cago}</h2>
-            <h3>Idade: {this.props.idade}</h3>
-        </div>
+           <div>
+               <h1>Meu projeto{this.state.hora}</h1>
+           </div>
         )
     }
 }
 
-function App(){
-    return (
-        <div>
-           <h1>Conheça nossa equipe</h1>
-           <Equipe nome="Maik" cargo="Programador" idade="21"/>
-           <Equipe nome="William" cargo="Programador" idade="32"/>
-        </div>
-    )
-}
+
 
 export default App
