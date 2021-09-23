8bda7b57b86fecd8b846021b4ac5bae9e9cd85dd
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-22 22:39:56 -0300

    usando class

--- React/meusite/src/App.js
+++ React/meusite/src/App.js
@@ -1,36 +1,33 @@
-import React from 'react'
-const Equipe = (props)=>{
-    return (
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-            <Social face={props.face} insta={props.insta}/>
-        </div>
-    )
+import React,{Component} from 'react'
+class Equipe extends Component {
+    render() {
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo}
+                idade={this.props.idade}/>
+                <hr/>
+            </div>
+        )
+    }
 }
-
-const Sobre =(props)=>{
-    return (
-        <div>
-            <h2>Olá sou o (a){props.nome}</h2>
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+            <h2> Ola sou o Matheus{this.props.nome}</h2>
+            <h2>Cargo: {this.props.cago}</h2>
+            <h3>Idade: {this.props.idade}</h3>
         </div>
-    )
+        )
+    }
 }
-const Social =(props)=>{
-    return (
-        <div>
-            <h2>Meu Facebook é {props.face} e meu Insta{props.insta}</h2>
-        </div>
-    )
-}
-
 
 function App(){
     return (
         <div>
-            <h1>Conheça nossa Equipe</h1>
-            <Equipe nome="Maik" cargo="Programador"/>
-            <Social  face="facebook" insta="instagram"/>
-            
+           <h1>Conheça nossa equipe</h1>
+           <Equipe nome="Maik" cargo="Programador" idade="21"/>
+           <Equipe nome="William" cargo="Programador" idade="32"/>
         </div>
     )
 }
