e17489c8408bd7e1dcaf745767678af829faccc9
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-09-27 00:25:37 -0300

    signIn signUp

--- Javascript/Mini-projetos_js/sistemachamado/src/pages/SignIn/index.js
+++ Javascript/Mini-projetos_js/sistemachamado/src/pages/SignIn/index.js
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import './signin.css'
+import logo from '../../assets/logo.png'
 
 
 function SignIn() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-      <div >
-         <h1>Pagina de Login</h1>
-      </div>
+        <div className="container-center">
+            <div className="login">
+                <div className="logo-area">
+                    <img src={logo} alt="Sistema logo" />
+                </div>
+                <form action="">
+                    <h1>Entrar</h1>
+                    <input type="text" placeholder="email@email.com"/>
+                    <input type="password" placeholder="*********"/>
+                    <button></button>
+                </form>
+            </div>
+        </div>
     );
-  }
-  
-  export default SignIn
-  
+}
+
+export default SignIn
