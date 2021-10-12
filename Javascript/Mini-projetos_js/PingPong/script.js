709791b2cbb534fb07cf2b8f1b6e4c90c4014a3c
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-10-11 23:40:09 -0300

    ping pong

---
+++ Javascript/Mini-projetos_js/PingPong/script.js
@@ -0,0 +1,34 @@
+//elementos
+
+var vbtInicar
+var vbola
+var vcpu
+var vjogador
+var vPainletxtPontos
+//controle de animaçao
+var game, frame
+
+// Posiçoes
+var posBolaX, posBolaY
+var posJogadorX, posJogadorY, posCpuX, posCpuY
+
+//direçao de acordo co a tecla
+var dirJy
+
+//Posiçoes iniciais
+var posJogIniY = 180,posCpuIniY = 180,
+ posBolaIniX=475, posBolaIniY=240
+
+// Tamanhos
+var campoX=0, campoY=0,campoW=960,campoH= 500
+
+var barraW= 20, barraH=140,bolaW=20,bolaH=20
+
+//direcao
+var bolaX, bolaY
+var jogY=0, cpuY=0
+
+//VELOCIDADE
+ var velBola, velCpu, velJogador
+ var pontos=0
+ var tecla;
